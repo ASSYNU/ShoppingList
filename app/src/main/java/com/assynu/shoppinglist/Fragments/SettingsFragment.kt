10d0464a353e9881f7789c9b880e4f6e86f3b729
Assynu <dawid.bartczak05@gmail.com>
2022-01-27 18:53:35 +0100

    Fixed not changing list id

--- app/src/main/java/com/assynu/shoppinglist/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/assynu/shoppinglist/Fragments/SettingsFragment.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.assynu.shoppinglist.R
 import com.assynu.shoppinglist.users.Manager.setUserId
 import com.assynu.shoppinglist.BuildConfig
@@ -21,10 +23,10 @@ class SettingsFragment : PreferenceFragmentCompat(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+
         findPreference<Preference>("pref_version")?.summary = BuildConfig.VERSION_NAME
-
-//        findPreference<Preference>("ShareUserID")?.onPreferenceChangeListener = shareListId()
-
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
@@ -35,7 +37,7 @@ class SettingsFragment : PreferenceFragmentCompat(),
             val uid = getUserId(context)
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, "Someone shared you their shopping list id: " + uid)
+                putExtra(Intent.EXTRA_TEXT, "List id: $uid")
                 type = "text/plain"
             }
 
@@ -51,12 +53,25 @@ class SettingsFragment : PreferenceFragmentCompat(),
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        Log.v("Settings Debug", key)
         if (key == "UserID") {
             var newUID = sharedPreferences.getString("UserID", "")
 
+            Log.v("Settings Debug", "Not checked: $newUID")
+
             if (newUID == null || newUID == "") newUID = ""
 
+            Log.v("Settings Debug", "Checked: $newUID")
+
             setUserId(context, newUID)
+
+            if (newUID == "") {
+                val editor = sharedPreferences.edit()
+                editor.putString("UserID", getUserId(context))
+                editor.apply()
+
+                findNavController().navigate(R.id.action_settingsFragment_to_FirstFragment)
+            }
         }
     }
 }
